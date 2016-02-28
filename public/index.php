0be135b051107139c9be1ec451df5416b1d2d726
aqib <codekashmir@gmail.com>
2016-02-28 11:15:45 +0530

    added: ajax request for url shortening

--- public/index.php
+++ public/index.php
@@ -1,9 +1,19 @@
 <?php
-require_once __DIR__.'/../vendor/autoload.php';
-
-/*
-connect to database
- */
-$db = new CodeKashmir\UrlShortner\Db();
+if(!empty($_POST)){
+	require_once __DIR__.'/../vendor/autoload.php';
+	/*
+	connect to database
+	 */
+	$db = new CodeKashmir\UrlShortner\Db();
+	/*
+	 instantiate Url shortener service
+	*/
+	$urlShortner = new CodeKashmir\UrlShortner\UrlShortner();
+	/*
+		output shortened Url
+	*/
+	echo $urlShortner->shortn($_POST['siteurl']);
+	die();
+}
 
 echo file_get_contents('./home.html');
--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,27 @@
 var app = angular.module('UrlShortner', ['ngMaterial', 'ngMessages']);
 
-app.controller('AppCtrl',['$http','$scope', '$log',function($http, $scope, $log){
-	//holds urls
-	$scope.siteurl = "http://";
-	//submit this url for shortening
-	$scope.submitURL = function(){
-	
+app.controller('AppCtrl',['$http','$scope', '$log', '$httpParamSerializerJQLike',
+ 	function($http, $scope, $log, $httpParamSerializerJQLike){
+		//holds urls
+		$scope.siteurl = "http://";
+		//submit this url for shortening
+		$scope.submitURL = function(){
+			$scope.isShortening = true;
+			var data = {
+				'siteurl' : $scope.siteurl
+			}
+			//send ajax request
+			$http({
+				method: 'POST',
+				url: '/',
+				data: $httpParamSerializerJQLike(data),
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			}).success(function(response){
+				$log.info(response);
+				$scope.isShortening = true;
+			}).error(function(response){
+				$scope.isShortening = false;
+			});
+		}
 	}
-}]);
+]);
