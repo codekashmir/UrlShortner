9df3cd569b40327aa4d8d1b29c93c9c53f85e8ef
aqib <codekashmir@gmail.com>
2016-02-28 09:59:47 +0530

    added: angular controller

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
 var app = angular.module('UrlShortner', ['ngMaterial', 'ngMessages']);
+
+app.controller('AppCtrl',['$http','$scope', '$log',function($http, $scope, $log){
+	//holds urls
+	$scope.siteurl = "http://";
+	//submit this url for shortening
+	$scope.submitURL = function(){
+	
+	}
+}]);
